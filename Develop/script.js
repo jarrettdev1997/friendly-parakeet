e20cc2167579f4e96cc21770e859133d707a24f1
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-10-31 23:23:05 -0400

    random array added to fxns

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+const arrayFromLowToHigh = generatePassword
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -19,6 +20,29 @@ funtion arrayFromLowToHigh(low, high) {
     array.push(i)
   }
   return array
+}
+
+function generatePassword( len ) {
+  var length = (len)?(len):(10);
+  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+  var numeric = '0123456789';
+  var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var password = "";
+  var character = "";
+  var crunch = true;
+  while( password.length<length ) {
+      entity1 = Math.ceil(string.length * Math.random()*Math.random());
+      entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+      entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+      hold = string.charAt( entity1 );
+      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+      character += hold;
+      character += numeric.charAt( entity2 );
+      character += punctuation.charAt( entity3 );
+      password = character;
+  }
+  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+  return password.substr(0,len);
 }
 
 function writePassword() {

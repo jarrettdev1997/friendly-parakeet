909a5e706a0bae04879843f8f42075899419e5da
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-10-31 21:09:56 -0400

    password generation fxns added

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,26 @@
 // Assignment code here
-var passwordLength = window.prompt("How many characters would you like your password to contain?")
-
-function passwordLength() {
-  
-}
-var charTypeSelector = window.prompt("")
-
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+  ).concat(
+    arrayFromLowToHigh(91, 96)
+  ).concat(arrayFromLowToHigh(123, 126)
+  )
+  console.log(SYMBOL_CHAR_CODES)
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input along with other fxns
+funtion arrayFromLowToHigh(low, high) {
+  const array =[]
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -20,4 +31,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  window.prompt(onClick)

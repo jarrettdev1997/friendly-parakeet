8ebbb0e227cb743f8fe28361ba76ef7464782670
jarrettdev1997 <72107773+jarrettdev1997@users.noreply.github.com>
2020-11-01 17:48:45 -0500

    Add files via upload
    Local files added to repo. An error was created when forking the base repo to the my remote github repo.

---
+++ script.js
@@ -0,0 +1,50 @@
+// Assignment code here
+const arrayFromLowToHigh = generatePassword
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+  ).concat(
+    arrayFromLowToHigh(91, 96)
+  ).concat(arrayFromLowToHigh(123, 126)
+  )
+  //console.log(SYMBOL_CHAR_CODES)
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input along with other fxns
+function generatePassword( len ) {
+  var length = (len)?(len):(10);
+  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+  var numeric = '0123456789';
+  var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+  var password = "";
+  var character = "";
+  var crunch = true;
+  while( password.length<length ) {
+      entity1 = Math.ceil(string.length * Math.random()*Math.random());
+      entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
+      entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
+      hold = string.charAt( entity1 );
+      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+      character += hold;
+      character += numeric.charAt( entity2 );
+      character += punctuation.charAt( entity3 );
+      password = character;
+  }
+  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+  return password.substr(0,len);
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
